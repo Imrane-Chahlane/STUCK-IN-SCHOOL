1d52488bead53022603d4199a30db6d7c76a2908
Imrane-Chahlane <imranechahlane@gmail.com>
2026-04-10 20:09:55 +0100

    small prototype

---
+++ STUCK IN SCHOOL/Assets/Scripts/Player/MouseLook.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 200f;
+    public Transform playerBody;
+
+    private float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        if (playerBody != null)
+        {
+            playerBody.Rotate(0f, mouseX, 0f);
+        }
+    }
+}
