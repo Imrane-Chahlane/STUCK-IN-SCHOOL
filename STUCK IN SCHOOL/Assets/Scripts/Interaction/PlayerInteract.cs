bf1d7f17399562af1a17228b4d0366106946d8f3
cs-hmz <hamzasbusacc1@gmail.com>
2026-05-05 08:31:42 +0100

    version a tester

--- STUCK IN SCHOOL/Assets/Scripts/Interaction/PlayerInteract.cs
+++ STUCK IN SCHOOL/Assets/Scripts/Interaction/PlayerInteract.cs
@@ -1,14 +1,45 @@
 using UnityEngine;
-
+using TMPro;
 public class PlayerInteract : MonoBehaviour
 {
     public Camera playerCamera;
-    public float interactDistance = 40f;
+    public float interactDistance = 60f;
 
     public GameObject interactUI; // 👈 UI reference
 
     private Interactable currentInteractable;
 
+    private void Start()
+    {
+        if (playerCamera == null)
+            playerCamera = Camera.main;
+
+        // Chercher dans tous les Canvas même désactivés
+        if (interactUI == null)
+        {
+            // Chercher par nom
+            interactUI = GameObject.Find("IntercatableText");
+            
+            // Si pas trouvé, chercher dans tous les objets
+            if (interactUI == null)
+            {
+                TextMeshProUGUI[] allTexts = FindObjectsByType<TextMeshProUGUI>(
+                    FindObjectsInactive.Include, 
+                    FindObjectsSortMode.None);
+                foreach (var t in allTexts)
+                {
+                    if (t.gameObject.name == "IntercatableText")
+                    {
+                        interactUI = t.gameObject;
+                        break;
+                    }
+                }   
+            }
+        }
+    
+        if (interactUI != null)
+            interactUI.SetActive(false);
+    }
     private void Update()
     {
         CheckForInteractable();
